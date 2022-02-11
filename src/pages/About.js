d6e236eea54f9ae37e411013edac4d9750f366e4
jhajdari <jhajdari@ritech.co>
2022-02-11 12:14:25 +0100

    cocktail-app

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title" style={{ color: "white" }}>
+        about us
+      </h1>
+      <p style={{ color: "white" }}>
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit...Neque porro quisquam est qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit...Neque porro quisquam
+        est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+        velit...
+      </p>
+    </section>
+  );
+};
+
+export default About;
